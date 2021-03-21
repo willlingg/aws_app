f0f34c52a5fbb93343abe8ff7fa4acb5de5fa510
William Ling <will.ling1196@gmail.com>
2021-03-21 14:54:35 +1100

    Changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -11,6 +11,22 @@ function App() {
       </header>
     </div>
   );
+  import React from "react";
+  import logo from "./logo.svg";
+  import "./App.css";
+
+  function App() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Hello from V2</h1>
+        </header>
+      </div>
+    );
+  }
+
+  export default App;
 }
 
 export default App;
