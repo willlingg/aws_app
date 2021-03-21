c6ad3b99bcda7fa836486a8846640fd0f40e7494
William Ling <will.ling1196@gmail.com>
2021-03-21 14:57:52 +1100

    changes v2

--- src/App.js
+++ src/App.js
@@ -11,22 +11,6 @@ function App() {
       </header>
     </div>
   );
-  import React from "react";
-  import logo from "./logo.svg";
-  import "./App.css";
-
-  function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>Hello from V2</h1>
-        </header>
-      </div>
-    );
-  }
-
-  export default App;
 }
 
 export default App;
